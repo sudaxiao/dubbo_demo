6de5797e5a749516d8481a2fbebf8643ddb927dd
xiaosuda <xiaosuda@2dfire.com>
2017-12-19 14:30:55 +0800

    说明

--- restapi/src/main/java/com/dfire/vo/Student.java
+++ restapi/src/main/java/com/dfire/vo/Student.java
@@ -12,5 +12,9 @@ public class Student extends Person {
 
     private Integer age;
     private String name;
+    /**
+     *  查看序列化源码可知：
+     *  由于此字段与父类字段一样，在反序列化时会被父类的null值所覆盖，最终导致为null
+     */
     private String sex;
 }
